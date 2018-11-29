bf5cab3502abad43e7e81c61f76b2ae62d6f8ee0
Eselter <krzychu007@gmail.com>
2018-11-29 13:42:14 +0100

    fork projektu DemoING

---
+++ src/main/java/pl/b2b/net/ingCollectionsTest/pages/transactionPage/TestTransactionPage.java
@@ -0,0 +1,30 @@
+package pl.b2b.net.ingCollectionsTest.pages.transactionPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pl.b2b.net.ingCollectionsTest.SingletonWebDriver;
+
+public class TestTransactionPage {
+    private SingletonWebDriver singletonWebDriver;
+    private WebDriver webDriver;
+    private WebDriverWait wait;
+    private ObjectsTransationPage objectsTransationPage;
+
+    @BeforeClass
+    private void setUp() {
+        singletonWebDriver = SingletonWebDriver.getInstance();
+        webDriver = singletonWebDriver.getWebDriver();
+        wait = singletonWebDriver.getWait();
+        objectsTransationPage = new ObjectsTransationPage(webDriver, wait);
+    }
+
+    @Test
+    @Parameters({"nazwaKonta"})
+    public void selectAccountTest(@Optional("KONTO Direct") String nazwaKonta){
+        objectsTransationPage.wybierzKontoUzytkownika(nazwaKonta);
+    }
+}
